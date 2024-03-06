717a736ba5f79b8b6b996fb1445e4ac1d49b80eb
Tiffany Chambers-Samuels <148689563+TiffCS@users.noreply.github.com>
2024-03-06 11:07:23 +0000

    Adding Identity, JWT-Token and Roles

--- Controllers/StocksController.cs
+++ Controllers/StocksController.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using InventoryManagement.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace InventoryManagement.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Purchasing")]
+    [Authorize(Roles = "Inventory")]
     public class StocksController : ControllerBase
     {
         private readonly InventoryManagementContext _context;
