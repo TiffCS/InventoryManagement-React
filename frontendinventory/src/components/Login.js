ba0b6de48d2ece07924b36a82a3c95376deeac7c
Tiffany Chambers-Samuels <148689563+TiffCS@users.noreply.github.com>
2024-04-22 11:14:19 +0100

    Using contextAPI to get user authentication

--- frontendinventory/src/components/Login.js
+++ frontendinventory/src/components/Login.js
@@ -1,102 +1,98 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import AuthContext from "../authContext/AuthProvider";
 import { useNavigate, Link } from "react-router-dom";
 import axios from 'axios';
 import { API_BASE_URL } from '../apiConfig';
 
+
 const Login = () => {
+
+        // Sets state for authenitication 
+        const {setAuth} = useContext(AuthContext);
+
+        // State for email and password 
         const[email, setEmail] = useState('');
         const[password, setPassword] = useState('');
         const[errMsg, setErrMsg] = useState('');
 
-        //Clear error message when user enters input
+        // Clear error message when user enters input
         useEffect(() => {
         setErrMsg('');
         }, [email, password])
 
+        // Navigation to endpoints
         const navigate=useNavigate()
 
-        const handleLogin = (e) => {
-        e.preventDefault();
-        console.log(email, password)
+        // Actions to be performed when when logging in
+        const handleLogin = async (e) => {
+            e.preventDefault();
+            console.log(email, password)
+            setEmail('');
+            setPassword('');
+            
 
-        const data = {
-            email: email,
-            password: password
-        }
-        console.log(data);
-
-        axios.post(`${API_BASE_URL}account/login`, data)
-        .then((result) => {
-            console.log(result)
-            navigate('/home');
-        })
-        .catch((error) => {
-            if(!error?.response) {
-                setErrMsg('No server response.');
-            } else if (error.response?.status === 400) {
-                setErrMsg('Missing email or password.');
-            } else if (error.response?.status === 401)
-            {
-                setErrMsg('Invalid login attempt.')
-            } else {
-                setErrMsg('Login Failed.')
+            try {
+                const response = await axios.post(`${API_BASE_URL}account/login`, {email:email, password:password})
+                // .then((response) => {
+                //    console.log(response.data)
+                // //     //navigate('/home');
+                // })
+                console.log(JSON.stringify(response?.data));
+                const token = JSON.stringify(response?.data?.token);
+                //console.log(token);
+                setAuth({email, password, token});
+                navigate('/home');
+                console.log(token);
+                
+            } catch(error){
+                if(!error?.response) {
+                    setErrMsg('No response from server.');
+                } else if (error.response?.status === 400) {
+                    setErrMsg('Missing email or password.');
+                } else if (error.response?.status === 401)
+                {
+                    setErrMsg('Unauthorized login attempt.')
+                } else {
+                    setErrMsg('Login Failed.')
+                }
+                console.log(error);
             }
-            console.log(error);
-        })
         }
-
-        
-    
-        
     
         return (
-            <div>
-               <form>
-                    <div className="offset-lg-3 col-lg-6">
-                        <form className ="container">
-                            <div className="card">
-                                <div className="card-header">
-                                <p className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
-                                <h1>User Login</h1>
-                                </div>
-                                <div className="card-body">
-                                    <div>
-                                        <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-                                        <input required
-                                        type="email" 
-                                        class="form-control" 
-                                        id="exampleInputEmail1" 
-                                        aria-describedby="emailHelp" 
-                                        placeholder="Please enter email"
-                                        onChange= {(e) => setEmail(e.target.value)}
-                                        />
+            <section>
+                <p className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
+                <h1>User Login</h1>
+                <form onSubmit={handleLogin}>
+                    <label for="InputEmail" class="form-label mt-4">Email address</label>
+                        <input required
+                            type="email" 
+                            class="form-control" 
+                            id="InputEmail" 
+                            autoFocus
+                            aria-describedby="emailHelp" 
+                            placeholder="Please enter email"
+                            onChange= {(e) => setEmail(e.target.value)}
+                            value={email}
+                        />
                                         
-                                    </div>
-                                    <div>
-                                        <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-                                        <input required
-                                        type="password" 
-                                        class="form-control" 
-                                        id="exampleInputPassword1" 
-                                        placeholder="Enter password"
-                                        onChange= {(e) => setPassword(e.target.value)}
-                                        />
-                                        
-                                    </div>
-                                <div className="card-footer">
-                                <button type="Login" class="btn btn-primary" onClick={(e) => handleLogin(e)}>
-                                Login
-                                </button>
-                                <p>
-                                    Not Registered? <Link to ="/register">Register Here</Link>
-                                </p>
-                                </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </form>   
-            </div>
+                    <label for="InputPassword" class="form-label mt-4">Password</label>
+                        <input required
+                            type="password" 
+                            class="form-control" 
+                            id="InputPassword" 
+                            placeholder="Enter password"
+                            onChange= {(e) => setPassword(e.target.value)}
+                            value={password}
+                        />
+                    <button type="Login" class="btn btn-primary">
+                    Login
+                    </button>
+                    <p>
+                     Not Registered? <Link to ="/register">Register Here</Link>
+                    </p>
+                </form>
+            </section>
         );
 }
 
--- frontendinventory/src/components/Registration.js
+++ frontendinventory/src/components/Registration.js
@@ -6,7 +6,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-// Email and password patterns 
+// Email and password pattern to check email and password
 const emailRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
@@ -46,34 +46,41 @@ const Registration = () => {
         setErrMsg('');
     }, [email, password])
 
+    // Navigation to endpoints
     const navigate=useNavigate()
 
-    const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log(email, password)
+    // Actions to be performed when the form is submitted
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        console.log(email, password)
 
-    const data = {
-        email: email,
-        password: password
+        // Clear email and password input field
+        setEmail('');
+        setPassword('');
+
+        // Navigation  to login page if successful or error is displayed
+        try {
+            await axios.post(`${API_BASE_URL}account/register`, {email:email, password:password})
+            navigate('/login');
+        } catch(error) {
+            if (!error.response){
+                setErrMsg('No response from server.')
+            } else if (error.response?.status === 400)
+            {
+                setErrMsg('This email is already in use.')
+            } else { 
+                setErrMsg('User registration failed.')
+            }
+            
+            console.log(error);
+        }
     }
-    console.log(data);
-
-    axios.post(`${API_BASE_URL}account/register`, data)
-    .then((result) => {
-        
-        navigate('/login');
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-
-   }
 
     return (
         <section>
             <p className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
             <h1>User Registration</h1>
-           <form>
+           <form onSubmit={handleFormSubmit}>
                 <label for="InputEmail" class="form-label mt-4"> 
                     Email address
                     <span className={validEmail ? "valid" : "hide"}>
@@ -88,6 +95,7 @@ const Registration = () => {
                     type="email" 
                     class="form-control" 
                     id="InputEmail"
+                    value = {email}
                     autoFocus
                     autoComplete="off"
                     placeholder="@gmail.com"
@@ -115,6 +123,7 @@ const Registration = () => {
                     type="password" 
                     class="form-control" 
                     id="InputPassword" 
+                    value = {password}
                     placeholder="Enter password"
                     onChange= {(e) => setPassword(e.target.value)}
                     onFocus={() => setPasswordFocus(true)}
@@ -127,7 +136,7 @@ const Registration = () => {
                         Atleast one character has to be uppercase and lowercase. <br />
                         Password also requires a non-Aphanumeric character.
                 </p>              
-                <button type="Register" class="btn btn-primary" disabled={!validEmail || !validPassword} onClick={(e) => handleFormSubmit(e)}>
+                <button type="Register" class="btn btn-primary" disabled={!validEmail || !validPassword}>
                     Register
                 </button>
                 <p>
